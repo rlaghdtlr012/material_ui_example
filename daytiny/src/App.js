57a9b6deb5bf4cee32f8478653a0bb1fa8a79d6b
unknown <rlaghdtlr012@naver.com>
2023-06-19 23:59:10 +0900

    집에서 커밋. node 18.16.0 사용

---
+++ daytiny/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './assets/icons/logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="nav">
+        <img src={logo} /> DAYTINY
+      </div>
+    </div>
+  );
+}
+
+export default App;
